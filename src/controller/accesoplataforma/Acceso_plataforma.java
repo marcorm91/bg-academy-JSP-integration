ce036cff029bfdeb403bc1fed78ba9f02d1e64b4
Marco Romero <marco_antonio88_9@hotmail.com>
2017-05-04 21:23:25 +0200

    Añado controller -> accesoplataforma para realizar el control de logueo previo a la plataforma.

---
+++ src/controller/accesoplataforma/Acceso_plataforma.java
@@ -0,0 +1,44 @@
+package controller.accesoplataforma;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Acceso_plataforma
+ */
+@WebServlet(urlPatterns = { "/Acceso_plataforma" })
+public class Acceso_plataforma extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Acceso_plataforma() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.sendRedirect("acceso/principal-alumno.jsp");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
