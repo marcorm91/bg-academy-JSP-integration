21b72c53300a5e61cdc63a4430ce9b686e9a3ed6
Marco Romero <marco_antonio88_9@hotmail.com>
2017-06-19 18:13:02 +0200

    Reestructuración y formateo de código fuente.

--- WebContent/acceso/assets/js/a_mis-calificaciones.js
+++ WebContent/acceso/assets/js/a_mis-calificaciones.js
@@ -13,22 +13,22 @@ $(document).ready(function(){
 			for(var i = 0; i < resp.length; i++){
 				for(j = 0; j < 4; j++){
 					if(resp[i][j] == null){
-    					resp[i][j] = "";
-    				}
+    						resp[i][j] = "";
+    					}
 				}
 			}
 			
-           for(var i = 0; i < resp.length; i++){
-        	   
-               	$("#tareas").append("<div class='col-xs-12'>																					"+
-               						"	<h4 class='titulo-alumno-calif'>"+resp[i][1]+"</h4>														"+
-               						"	<h5 class='descripcion-alumno-calif text-justify'>"+resp[i][2]+"</h5>									"+
-               						"	<span class='fecha-alumno-calif'>"+$.datepicker.formatDate('dd/mm/yy', new Date(resp[i][3]))+"</span>  	"+
-               						"	<span class='nota-alumno-calif'>Nota: <span> - </span></span><hr/>										"+
-               						"</div>");
-           }
+		   for(var i = 0; i < resp.length; i++){
 
-		}
+			$("#tareas").append("<div class='col-xs-12'>																					"+
+					    "	<h4 class='titulo-alumno-calif'>"+resp[i][1]+"</h4>							"+
+					    "	<h5 class='descripcion-alumno-calif text-justify'>"+resp[i][2]+"</h5>					"+
+					    "	<span class='fecha-alumno-calif'>"+$.datepicker.formatDate('dd/mm/yy', new Date(resp[i][3]))+"</span>  	"+
+					    "	<span class='nota-alumno-calif'>Nota: <span> - </span></span><hr/>					"+
+					    "</div>");
+		   }
+
+	     }
 		
 	});
 	
@@ -44,21 +44,21 @@ $(document).ready(function(){
 			for(var i = 0; i < resp.length; i++){
 				for(j = 0; j < 4; j++){
 					if(resp[i][j] == null){
-    					resp[i][j] = "";
-    				}
+						resp[i][j] = "";
+					}
 				}
 			}
 			
-			var cont = resp.length;	
+		   var cont = resp.length;	
 			
-           for(var i = 0; i < resp.length; i++){
-        	   	            	   
-               	$("#examenes").append("<div class='col-xs-12'>																					"+
-               						"	<h4 class='titulo-alumno-calif'>"+resp[i][1]+"</h4>														"+
-               						"	<h5 class='descripcion-alumno-calif text-justify'>"+resp[i][2]+"</h5>									"+
-               						"	<span class='fecha-alumno-calif'>"+$.datepicker.formatDate('dd/mm/yy', new Date(resp[i][3]))+"</span>  	"+
-               						"	<span class='nota-alumno-calif'>Nota: <span> - </span></span><hr/></div>								");	               	
-           }
+		   for(var i = 0; i < resp.length; i++){
+
+			$("#examenes").append(  "<div class='col-xs-12'>											"+
+						"	<h4 class='titulo-alumno-calif'>"+resp[i][1]+"</h4>							"+
+						"	<h5 class='descripcion-alumno-calif text-justify'>"+resp[i][2]+"</h5>					"+
+						"	<span class='fecha-alumno-calif'>"+$.datepicker.formatDate('dd/mm/yy', new Date(resp[i][3]))+"</span>  	"+
+						"	<span class='nota-alumno-calif'>Nota: <span> - </span></span><hr/></div>				");	               	
+		   }
 
 		},
 		complete: function(){
